096f5c849103fc29d40240ed4889025a3d2ffb5c
Oussama Mangoub <mangoub4@gmail.com>
2022-01-15 04:20:55 +0100

    ...

--- src/projectPresent/Home.js
+++ src/projectPresent/Home.js
@@ -3,9 +3,16 @@ import Navbar from "./Navbar";
 import "./Style.css";
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div>
+        <img
+          src="http://www.inpt.ac.ma/sites/default/files/ifra%202.png"
+          width="100%"
+          height="450px"
+        />
+      </div>
+    </>
   );
 };
 
