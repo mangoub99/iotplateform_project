4cf63fb7bf644c7efc0d929a32f78bf428b67722
Oussama Mangoub <mangoub4@gmail.com>
2022-01-15 04:21:55 +0100

    Update Home.js

--- src/projectPresent/Home.js
+++ src/projectPresent/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
           src="http://www.inpt.ac.ma/sites/default/files/ifra%202.png"
           width="100%"
           height="450px"
+          alt="inpt"
         />
       </div>
     </>
